78fb41290a3c70ccef9ebe7c38a94c564b43eb18
04alexklink <alex_klink123@hotmail.co.uk>
2020-11-01 17:33:56 +0000

    Added minimum temperature as a property in the object constructor function. Changed if statement in tempDown() to use this.MINIMUM_TEMPERATURE. This is intended to be seen as a constant value

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,7 +10,6 @@ describe('Thermostat', function() {
   it('has a temp of 20 degrees when initialised', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   })
-
   it('increases temperature with tempUp()', function() {
     thermostat.tempUp();
     expect(thermostat.getCurrentTemperature()).toEqual(21);      
@@ -18,5 +17,11 @@ describe('Thermostat', function() {
   it('decreases temperature with tempDown()', function() {
     thermostat.tempDown();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
+  })
+  it('has minimum temp of 10 degrees', function() {
+    for(let i = 0; i <11; i ++) {
+      thermostat.tempDown();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
   })
 })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,17 +1,17 @@
 class Thermostat {
   constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
   }
-
   getCurrentTemperature() {
     return this.temperature;
   }
-
   tempUp() {
     this.temperature += 1;
   }
-
   tempDown() {
+    if(this.temperature > this.MINIMUM_TEMPERATURE) {
     this.temperature -= 1;
+    }
   }
 }
