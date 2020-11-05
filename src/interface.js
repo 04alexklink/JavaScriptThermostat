5132a6b61ea64a6acce254d943d199f0e17f5e26
04alexklink <alex_klink123@hotmail.co.uk>
2020-11-05 16:49:24 +0000

    Connected a weather API to interface.js. Using AJAX request london weather, and use the temp returned from this object that is returned to set text for a new local temperature element in the html. So when page loads, this request is made and local temperature is loaded in the page

--- src/interface.js
+++ src/interface.js
@@ -6,15 +6,32 @@ resetTemp = document.getElementById('reset-temp');
 turnPSMOff = document.getElementById('PSM-off');
 turnPSMOn = document.getElementById('PSM-on');
 
+
+// AJAX request for weather for specific locations
+localTemp = document.getElementById('local-temperature');
+var weather;
+function getLocalWeather() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', true);
+  xhr.onload = function() {
+  weather = JSON.parse(this.responseText);
+  localTemp.innerHTML= `${weather.main.temp}`
+  }
+  xhr.send();
+};
+
+getLocalWeather();
+
+
 var thermostat = new Thermostat();
 currentTemp.innerHTML = `${thermostat.getCurrentTemperature()}`;
 currentTemp.className = `${thermostat.displayUsage()}`;
 PSMStatus.innerHTML = `Power Saving Mode: On`;
 
 tempUp.addEventListener('click', function() {
-    thermostat.tempUp();
-    updateTemperature();   
-  });
+  thermostat.tempUp();
+  updateTemperature();   
+});
 
 tempDown.addEventListener('click', function() {
   thermostat.tempDown();
