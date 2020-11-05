bdfe7072809ff3d3a365e04c9a6df4d3794120e6
04alexklink <alex_klink123@hotmail.co.uk>
2020-11-05 11:51:11 +0000

    When page is loaded, want a new instance of thermostat to be created. And text to be set for PSM status and current temperature elements. Removed hardcoded text for both these elements from index.html

--- src/interface.js
+++ src/interface.js
@@ -5,3 +5,7 @@ tempDown = document.getElementById('temp-down');
 resetTemp = document.getElementById('reset-temp');
 turnPSMOff = document.getElementById('PSM-off');
 turnPSMOn = document.getElementById('PSM-on');
+
+var thermostat = new Thermostat();
+currentTemp.innerHTML = `${thermostat.getCurrentTemperature()}`;
+PSMStatus.innerHTML = `Power Saving Mode: On`;
