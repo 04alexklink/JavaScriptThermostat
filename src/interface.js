630e26df7988f65d4950a6af2010921ecc636f64
04alexklink <alex_klink123@hotmail.co.uk>
2020-11-06 22:04:37 +0000

    Refactor. for DRY principles, making 3 AJAX API requests with identical code. Therefore put the identical code (responsible for making the url get request and outputting returned data into html for city and current weather) into a function that takes in a parameter of city (getting the city input is the difference between all 3 functions, so still need separate functions, but they now all call displayWeather(city). For default London, that is now just displayWeather('London')

--- src/interface.js
+++ src/interface.js
@@ -10,34 +10,27 @@ turnPSMOn = document.getElementById('PSM-on');
 // AJAX request for London weather from an API. Loads to page by default:
 localTemp = document.getElementById('local-temperature');
 localCity = document.getElementById('city');
-dropDownLocalCitySelection = document.getElementById('dropdown-current-city');
-var weather;
-function getLondonWeather() {
+
+function displayLocalWeather(city) {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', true);
-  xhr.onload = function() {
-    weather = JSON.parse(this.responseText);
-    localTemp.innerHTML= `${weather.main.temp}`
-    localCity.innerHTML= `London`
-  }
-  xhr.send();
-};
-
-getLondonWeather();
-
-// get Local Weather for one of 4 cities available in a drop down menu through AJAX request to weather API:
-dropDownLocalCitySelection.addEventListener('change', getLocalWeather); 
-
-function getLocalWeather() {
-  var city = dropDownLocalCitySelection.value;
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET',`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`, true);
+  xhr.open('GET', `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`, true);
   xhr.onload = function() {
     var weather = JSON.parse(this.responseText);
     localTemp.innerHTML= `${weather.main.temp}`;
     localCity.innerHTML= `${city}`;
   }
   xhr.send();
+};
+
+displayLocalWeather('London');
+
+// get Local Weather for one of 4 cities available in a drop down menu through AJAX request to weather API:
+dropDownLocalCitySelection = document.getElementById('dropdown-current-city');
+dropDownLocalCitySelection.addEventListener('change', dropDownGetLocalCityWeather); 
+
+function dropDownGetLocalCityWeather() {
+  var city = dropDownLocalCitySelection.value;
+  displayLocalWeather(city);
 };
 
 // get local weather for any city the user types in, sending AJAX request to weather API:
@@ -45,15 +38,8 @@ submittedLocalCitySelection = document.getElementById('submitted-select-city');
 submittedLocalCitySelection.addEventListener('submit', function(e) {
   e.preventDefault();
   var city = document.getElementById('select-current-city').value;
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET',`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`, true);
-  xhr.onload = function() {
-    var weather = JSON.parse(this.responseText);
-    localTemp.innerHTML= `${weather.main.temp}`;
-    localCity.innerHTML= `${city}`;
-  }
-  xhr.send();
-})
+  displayLocalWeather(city);
+});
 
 var thermostat = new Thermostat();
 currentTemp.innerHTML = `${thermostat.getCurrentTemperature()}`;
